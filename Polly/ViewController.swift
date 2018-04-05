ad9bf8062bbc4c93c1f67ce60fbe1f03c14319b3
Helen Kim <hyejunk@andrew.cmu.edu>
2018-04-05 18:54:31 -0400

    Initial Commit

---
+++ Polly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polly
+//
+//  Created by Helen Kim on 4/5/18.
+//  Copyright © 2018 translators. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
