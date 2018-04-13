438653bcafbbc186cf0d7bff7613bb49e1bcba6c
Helen Kim <hyejunk@andrew.cmu.edu>
2018-04-12 22:16:35 -0400

    assets added

---
+++ Polly/StepOne.swift
@@ -0,0 +1,13 @@
+//
+//  StepOne.swift
+//  Polly
+//
+//  Created by Helen Kim on 4/7/18.
+//  Copyright © 2018 translators. All rights reserved.
+//
+
+import UIKit
+
+class StepOne : UIViewController {
+    
+}
--- Polly/ViewController.swift
+++ Polly/ViewController.swift
@@ -1,25 +1,49 @@
 //
 //  ViewController.swift
-//  Polly
+//  HorizontalScrollView
 //
-//  Created by Helen Kim on 4/5/18.
-//  Copyright © 2018 translators. All rights reserved.
+//  Created by Yash Patel on 04/10/17.
+//  Copyright © 2017 Yash Patel. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var contentWidth:CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        
+        for image in 0...2 {
+            let imageToDisplay = UIImage(named: "\(image).png")
+            let imageView = UIImageView(image: imageToDisplay)
+            
+            let xCoordinate = view.frame.midX + view.frame.width * CGFloat(image)
+            contentWidth += view.frame.width
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: xCoordinate - 50, y: (view.frame.height / 2) + 50, width: 100, height: 100)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
     }
-
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(414))
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
