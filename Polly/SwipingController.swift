da5f69d326bafffad40da2d0dbe6ef36b7abb49a
Sophie Zhao <syzhao@andrew.cmu.edu>
2018-04-15 15:39:41 -0400

    swipe collection tutorial vertical

---
+++ Polly/SwipingController.swift
@@ -0,0 +1,34 @@
+//
+//  SwipingController.swift
+//  Polly
+//
+//  Created by Sophie Zhao on 4/15/18.
+//  Copyright © 2018 translators. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Return number of crash course pages we need
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+}
