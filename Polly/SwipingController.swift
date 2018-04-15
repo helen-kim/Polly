206487105dde8bd35fa38d3bcb6ebf8919c8bce5
Sophie Zhao <syzhao@andrew.cmu.edu>
2018-04-15 15:49:24 -0400

    tutorial - horizontal

--- Polly/SwipingController.swift
+++ Polly/SwipingController.swift
@@ -13,18 +13,24 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
         collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
+        collectionView?.isPagingEnabled = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // Return number of crash course pages we need
-        return 4
+        return 4 
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = .red
+        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .white
         return cell
     }
     
