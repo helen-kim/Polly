d157c72c9b408d6c84e1035f4a78163d1bb88828
Sophie Zhao <syzhao@andrew.cmu.edu>
2018-04-15 22:19:10 -0400

    pagecell

---
+++ Polly/PageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PageCell.swift
+//  Polly
+//
+//  Created by Sophie Zhao on 4/15/18.
+//  Copyright © 2018 translators. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
