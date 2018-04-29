7d1e7b39f8b8c17eafeb790cadea6013ebdbbfb2
Sophie Zhao <syzhao@andrew.cmu.edu>
2018-04-29 15:03:56 -0400

    comments to see helens changes

--- Polly/PageCell.swift
+++ Polly/PageCell.swift
@@ -1,20 +1,21 @@
+////
+////  PageCell.swift
+////  Polly
+////
+////  Created by Sophie Zhao on 4/15/18.
+////  Copyright © 2018 translators. All rights reserved.
+////
 //
-//  PageCell.swift
-//  Polly
+//import UIKit
 //
-//  Created by Sophie Zhao on 4/15/18.
-//  Copyright © 2018 translators. All rights reserved.
+//class PageCell: UICollectionViewCell {
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        backgroundColor = .purple
+//    }
 //
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
 
-import UIKit
-
-class PageCell: UICollectionViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .purple
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
--- Polly/SwipingController.swift
+++ Polly/SwipingController.swift
@@ -1,45 +1,46 @@
+////
+////  SwipingController.swift
+////  Polly
+////
+////  Created by Sophie Zhao on 4/15/18.
+////  Copyright © 2018 translators. All rights reserved.
+////
 //
-//  SwipingController.swift
-//  Polly
+//import UIKit
 //
-//  Created by Sophie Zhao on 4/15/18.
-//  Copyright © 2018 translators. All rights reserved.
+//class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 //
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        collectionView?.backgroundColor = .white
+//        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+//
+//        collectionView?.isPagingEnabled = true
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        // Return number of crash course pages we need
+//        return 4
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+//
+//        // dont try this apparently
+////        let imageView = UIImageView()
+////        cell.addSubview(imageView)
+//
+////        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .white
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: view.frame.width, height: view.frame.height)
+//    }
+//}
 
-import UIKit
-
-class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        collectionView?.backgroundColor = .white
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
-        
-        collectionView?.isPagingEnabled = true
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // Return number of crash course pages we need
-        return 4 
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-
-        // dont try this apparently
-//        let imageView = UIImageView()
-//        cell.addSubview(imageView)
-        
-        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .white
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
-    }
-}
