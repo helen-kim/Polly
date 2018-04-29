7b2635e0709d9f414255f1566920e5715fee0572
Helen Kim <hyejunk@andrew.cmu.edu>
2018-04-29 16:00:10 -0400

    connecting pickerview

--- Polly/SpecificationsController.swift
+++ Polly/SpecificationsController.swift
@@ -10,19 +10,29 @@ import UIKit
 
 extension UITextField {
     func setPadding() {
-        let paddingView = UIView(frame: CGRect(x: 0, y:0, width:10, height: self.frame.height))
+        let paddingView = UIView(frame: CGRect(x: 0, y:0, width:20, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = .always
-        
     }
 }
 
-class SpecificationsController: UIViewController {
+class SpecificationsController: UIViewController, UIPickerViewDelegate, UIPickerViewaDataSource {
 
     @IBOutlet weak var otherField: UITextField!
+    @IBOutlet weak var genderPicker: UIPickerView!
+    @IBOutlet weak var genderButton: RoundedButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         otherField.setPadding()
     }
+    
+    @IBAction func genderPressed(_ sender: RoundedButton) {
+        
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
 }
