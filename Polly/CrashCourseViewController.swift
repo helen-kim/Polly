8572e4f9efc9950ac33e7814dfad0b5d88bc72cc
Sophie Zhao <syzhao@andrew.cmu.edu>
2018-05-01 22:37:46 -0400

    1st tutorial done

--- Polly/CrashCourseViewController.swift
+++ Polly/CrashCourseViewController.swift
@@ -10,58 +10,47 @@ import UIKit
 
 class CrashCourseViewController: UIViewController {
 
-    let crashCourseImageView: UIImageView = {
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "capstone v1-1"))
-        // this enables autolayout for our imageview
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
+    @IBOutlet weak var crashCourseImageView: UIImageView!
     
-    let descriptionTextView: UITextView = {
-        let textView = UITextView()
-        textView.text = "Crash Course text here"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.textAlignment = .center
-        return textView
-    }()
+    @IBOutlet weak var titleTextView: UITextView!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Crash Course Image View Settings
+        self.crashCourseImageView.image = UIImage(named: "capstone v1-01.png")
+        self.crashCourseImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Title Text View Settings
+        self.titleTextView.font = UIFont.boldSystemFont(ofSize: 18)
+        self.titleTextView.translatesAutoresizingMaskIntoConstraints = false
+        self.titleTextView.textAlignment = .center
+        self.titleTextView.isEditable = false
+        self.titleTextView.isScrollEnabled = false
+        
+        // Description Text View Settings
+        self.descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionTextView.textAlignment = .center
+        self.descriptionTextView.isEditable = false
+        self.descriptionTextView.isScrollEnabled = false
+        
+        // Add subviews
         view.addSubview(crashCourseImageView)
-        view.addSubview(descriptionTextView)
+        view.addSubview(titleTextView)
         
         setupLayout()
 
         // Do any additional setup after loading the view.
     }
     
+    func viewWillAppear() {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
     private func setupLayout() {
-        let topImageContainerView = UIView()
-        //        topImageContainerView.backgroundColor = .blue
-        view.addSubview(topImageContainerView)
-        //enable auto layout
-        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        
-        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        
-        topImageContainerView.addSubview(crashCourseImageView)
-        crashCourseImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
-        crashCourseImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
-        crashCourseImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
-        
-        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
-        
-        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        
+
     }
 
     override func didReceiveMemoryWarning() {
